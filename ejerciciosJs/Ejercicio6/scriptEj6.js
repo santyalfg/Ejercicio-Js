ac26702baf4cadb8e842fd0f5ba1beb4c3111c3f
David Santiago Alfonso Guzman <santyalfg@gmail.com>
2025-02-21 22:08:47 -0500

    v1 Original

---
+++ ejerciciosJs/Ejercicio6/scriptEj6.js
@@ -0,0 +1,19 @@
+function calculateAverage() {
+    let total = 0;
+    let numberOfGrades = 4;
+
+    for (let i = 1; i <= numberOfGrades; i++) {
+        let grade = parseFloat(prompt(`Ingrese la nota ${i} (entre 1 y 5):`));
+        
+        // Validate the input
+        while (isNaN(grade) || grade < 1 || grade > 5) {
+            grade = parseFloat(prompt(`Entrada inválida. Ingrese la nota ${i} (entre 1 y 5):`));
+        }
+        
+        total += grade;
+    }
+
+    let average = total / numberOfGrades;
+    document.getElementById("resultado").innerText = `El promedio de las notas es: ${average.toFixed(2)}`;
+
+}
